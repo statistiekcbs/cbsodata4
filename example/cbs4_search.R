a0ab09fe7ff1553775ebab6e7f80f9f87ee1b5eb
Edwin de Jonge <edwindjonge@gmail.com>
2022-03-10 16:26:57 +0100

    Update cbs4_search.R
    Improved example

--- example/cbs4_search.R
+++ example/cbs4_search.R
@@ -1,8 +1,8 @@
 if (interactive()){
 
   ds_nl <- cbs4_search("geboorte", language="nl-nl")
-  ds_nl[1:3, c("Identifier", "Title")]
+  ds_nl[1:3, c("Identifier", "Title", "rel")]
 
   bike_tables <- cbs4_search("fiets")
-  bike_tables[1:10, c("Identifier", "Title")]
+  bike_tables[1:10, c("Identifier", "Title", "rel")]
 }
