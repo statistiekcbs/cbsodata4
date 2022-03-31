2fd2d0cf525a6064b60fc2548807be3f62923bc1
Edwin de Jonge <edwindjonge@gmail.com>
2022-03-31 16:00:58 +0200

    updating url

---
+++ NEWS.md
@@ -0,0 +1,4 @@
+# cbsodata4 0.9.3
+
+* Added a `NEWS.md` file to track changes to the package.
+* Updated the odata4 url to https://datasets.cbs.nl/odata 
--- R/package.R
+++ R/package.R
@@ -1,3 +1,4 @@
 #BASEURL4 <- "http://beta.opendata.cbs.nl/OData4"
 #BASEURL4 <- "https://beta-odata4.cbs.nl"
-BASEURL4 <- "https://odata4.cbs.nl"
+#BASEURL4 <- "https://odata4.cbs.nl"
+BASEURL4 <- "https://datasets.cbs.nl/odata"
